e1942134e2510f829b07563a4885abf4c8814acc
mirarj <44106058+mirarj@users.noreply.github.com>
2023-04-26 14:13:48 -0400

    heroku

--- Assignment14_StockTicker/hello_app.js
+++ Assignment14_StockTicker/hello_app.js
@@ -2,8 +2,9 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 console.log("This goes to the console window");
 http.createServer(function (req, res) {
+  console.log("This goes to the console window");
   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write("<h2>Hello World</h2>");
-   res.write ("Success!  This app is deployed online");
-   res.end();
+  res.write("<h2>Hello World</h2>");
+  res.write ("Success!  This app is deployed online");
+  res.end();
 }).listen(port);
